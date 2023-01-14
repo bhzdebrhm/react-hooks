f7698b9474387f500b0307e9f8f9790d8909c3ce
bhzdebrhm <bhzd.ebrhm@gmail.com>
2023-01-14 13:38:34 +0330

    add useObjectDepsEffect

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './useImmer';
 export * from './useTree';
 export * from './useRefHandle';
+export * from './useObjectDepsEffect';
---
+++ src/useObjectDepsEffect.ts
@@ -0,0 +1,21 @@
+import { DependencyList, EffectCallback, useEffect, useRef } from 'react';
+
+
+type DepsEqualFnType<TDeps extends DependencyList> = (prevDeps: TDeps, nextDeps: TDeps) => boolean;
+
+const useCustomCompareEffect = <TDeps extends DependencyList>(
+  effect: EffectCallback,
+  deps: TDeps,
+  depsEqual: DepsEqualFnType<TDeps>
+) => {
+
+  const ref = useRef<TDeps | undefined>(undefined);
+
+  if (!ref.current || !depsEqual(deps, ref.current)) {
+    ref.current = deps;
+  }
+
+  useEffect(effect, ref.current);
+};
+
+export default useCustomCompareEffect;
